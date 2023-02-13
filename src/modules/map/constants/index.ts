d91e6591f2ff487a52d36aed5d3e641a69d2cc46
Aleksandr Dergunov <dergunovs@mail.ru>
2023-02-13 22:14:43 +0300

    feat: cats, forest and mario items

--- src/modules/map/constants/index.ts
+++ src/modules/map/constants/index.ts
@@ -3,6 +3,9 @@ import { IMap } from '@/map/interface';
 import { amusement } from '@/map/data/amusement';
 import { horror } from '@/map/data/horror';
 import { training } from '@/map/data/training';
+import { mario } from '@/map/data/mario';
+import { cats } from '@/map/data/cats';
+import { forest } from '@/map/data/forest';
 
 export const MAP_URL = '/maps';
 
@@ -10,4 +13,7 @@ export const MAP_LIST: { [key: number]: IMap } = {
   1: amusement,
   0: training,
   2: horror,
+  3: mario,
+  4: cats,
+  5: forest,
 };
